088ecc82676a666e6102faf711e65c4aa8d95a4c
tanyachaudhary23 <tanyachaudhary2004@gmail.com>
2024-10-10 00:11:45 +0530

    Retrofit Integration

--- app/src/main/java/com/example/weatherapplication/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapplication/WeatherViewModel.kt
@@ -1,10 +1,38 @@
 package com.example.weatherapplication
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.weatherapplication.api.Constant
+import com.example.weatherapplication.api.NetworkResponse
+import com.example.weatherapplication.api.WeatherModel
+import kotlinx.coroutines.launch
 
 class WeatherViewModel : ViewModel() {
+    private val weatherApi = RetrofitInstance.weatherApi
+    private val _weatherResult = MutableLiveData<NetworkResponse<WeatherModel>>()
+    private val weatherResult : LiveData<NetworkResponse<WeatherModel>> = _weatherResult
     fun getData(city:String){
+        _weatherResult.value = NetworkResponse.Loading
+        viewModelScope.launch {
+            try {
+                val response = weatherApi.getWeather(Constant.apiKey, city)
+                if (response.isSuccessful) {
+                    response.body()?.let {
+                        _weatherResult.value = NetworkResponse.Success(it)
+                    }
+
+                } else {
+                    _weatherResult.value = NetworkResponse.Error("Failed to load data")
+                }
+            } catch (e: Exception) {
+                _weatherResult.value = NetworkResponse.Error("Failed to load data")
+
+            }
+
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/weatherapplication/api/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.weatherapplication.api
+
+object Constant {
+    val apiKey="385cfe59e3784b52a86120343241010"
+}
